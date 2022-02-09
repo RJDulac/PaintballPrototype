e0775493cefeab7c1fa17cfaabba55bb4de65554
Ryan Dulac <analyticrye@gmail.com>
2022-02-09 15:34:48 -0500

    Add project files.

---
+++ PaintBallPrototype/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace PaintBallPrototype
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PaintballGun gun = new PaintballGun();
+
+            while (true)
+            {
+                Console.WriteLine($"{gun.Balls} balls, {gun.BallsLoaded} loaded");
+                if (gun.IsEmpty()) { Console.WriteLine("WARNING: you're out of ammo"); }
+                Console.WriteLine("Space to shoot, r to reload, + to add ammo, q to quit");
+                Char key = Console.ReadKey(true).KeyChar;
+                if (key == ' ') { Console.WriteLine($"Shooting returned {gun.Shoot()}"); }
+                else if (key == 'r') { gun.Reload(); }
+                else if (key == '+') { gun.Balls += PaintballGun.MAGAZINE_SIZE; }
+                else if (key == 'q') { return; }
+            }
+        }
+    }
+}
